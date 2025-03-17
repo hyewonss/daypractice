0006d821b9a95cc6d1a79a43123201244d238fca
hyewonss <shwshw2003@soongsil.ac.kr>
2025-03-17 15:11:24 +0900

    Resolved merge conflicts and added new files

--- dprac/1010_다리놓기/1010.py
+++
@@ -1,9 +0,0 @@
-def fac(n):
-    if n <= 1:
-        return 1
-    return n*fac(n-1)
-
-t = int(input())
-for i in range(t):
-    n, m = map(int, input().split())
-    print(int(fac(m)/(fac(m-n)*fac(n))))
